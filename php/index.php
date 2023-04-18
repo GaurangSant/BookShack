bc2dd8ed09e842332b43e232a61199a3fc5c3b30
Gaurang <gaurang.sant612@gmail.com>
2023-04-18 21:54:02 +0530

    update

--- php/index.php
+++ php/index.php
@@ -8,21 +8,22 @@
     <link rel="stylesheet" href="home.css">
     <title>Home</title>
     <link rel="icon" type="image/x-icon" href="../images/image.png">
+    <link rel="stylesheet" href="../css/home.css">
 </head>
 
 <body>
     <div class="body">
         <div class="container">
-            <?php include ("navbar.html"); ?>
+            <?php include ("../html/navbar.html"); ?>
             <img src="../images/image1.jpg" alt="Books" width="1688" height="666">
-            <a href="store.php"><button class="button"><span>Explore</span></button></a>
+            <a href="./php/store.php"><button class="button"><span>Explore</span></button></a>
             <div class="centered">
                 <b> BUY AND SELL YOUR BOOKS AT BEST PRICE </b>
             </div>
         </div>
     </div>
     <footer>
-        <?php include ("footer.html"); ?>
+        <?php include ("../html/footer.html"); ?>
     </footer>
 </body>
 </html>
